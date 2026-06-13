49d6e5104679a7c9c39a8ff31edad2e443cfc541
syedsaied-ui <syedsaied26@gmail.com>
2026-06-13 06:24:22 +0500

    Updated project

--- 2-actual-backend/data/items.js
+++ 2-actual-backend/data/items.js
@@ -1,15 +1,28 @@
-const fs = require('node:fs/promises');
+const fs = require("node:fs/promises");
+const path = require("path");
+
+// go ONE level up from "data" folder
+const dataPath = path.join(__dirname, "..", "items.json");
 
 async function getStoredItems() {
-  const rawFileContent = await fs.readFile('items.json', { encoding: 'utf-8' });
-  const data = JSON.parse(rawFileContent);
-  const storedItems = data.items ?? [];
-  return storedItems;
+  try {
+    const rawFileContent = await fs.readFile(dataPath, "utf-8");
+    const data = JSON.parse(rawFileContent);
+    return data.items ?? [];
+  } catch (err) {
+    console.log("READ ERROR:", err.message);
+    return [];
+  }
 }
 
 function storeItems(items) {
-  return fs.writeFile('items.json', JSON.stringify({ items: items || [] }));
+  return fs.writeFile(
+    dataPath,
+    JSON.stringify({ items: items || [] }, null, 2),
+  );
 }
 
-exports.getStoredItems = getStoredItems;
-exports.storeItems = storeItems;
+module.exports = {
+  getStoredItems,
+  storeItems,
+};
